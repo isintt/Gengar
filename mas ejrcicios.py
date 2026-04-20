568fc06689b9e15c62f77ac35fca6ba663218143
isintt <isidorahuentecura@gmail.com>
2026-04-20 11:06:21 -0400

    ejercicios con string

--- mas ejrcicios.py
+++ mas ejrcicios.py
@@ -1,19 +1,68 @@
 
-entrenador="nose"
+entrenador=" nose "
 
 pkm1=6
 
-entrenador2="ñeñe"
+entrenador2="nini ni"
 pkm2=7
+
+
+# # tipos de datos
+# titulo="Clima actual" #tipo string
+# temp=18.6 #float
+# diaDelMes=16 #int
+# mes=4 #int
+# llueve=True #boolean
+
+# print(f"{titulo}")
+# print(f"Fecha de hoy: {diaDelMes}/{mes}")
+# print(f"Tempreatura actual {temp} grados")
 
 # if pkm1<=6:
 #     print("puede acceder a la liga pokemon")
 # else:
 #     print("tiene mas pokemons de los necesarios")
 
-print(len(entrenador))
+# print(len(entrenador))
 
-print("el entrenador", entrenador, "tiene", pkm1, " pokemons")
-print("el entrenador", entrenador2, "tiene", pkm2, " pokemons")
-print(f"el entrenador {entrenador2} \ntiene {pkm2} pokemons")
-print("el entrenador", entrenador2+ " tiene 7 pokemons")
+# print("el entrenador", entrenador, "tiene", pkm1, " pokemons")
+# print("el entrenador", entrenador2, "tiene", pkm2, " pokemons")
+# print(f"el entrenador {entrenador2} \ntiene {pkm2} pokemons")
+# print("el entrenador", entrenador2+ " tiene 7 pokemons")
+
+# print(f"hola {entrenador} "*3)
+#     -3-2-1
+# name="ANA"
+#     012
+# print(entrenador.strip()) 
+# print(entrenador.lower().strip())
+# print(entrenador2.upper())
+# print(entrenador2.replace("mi", "me"))
+# print(entrenador2.find("mi"))
+# print(entrenador2.split())
+
+
+# contr4="SHAZAM"
+# print("ingrese la clave")
+# code=input("ingrese la clave")
+# if contr4==code.upper():
+#     print("bienvenido al sistema")
+# else:
+#     print("error, clave invalida")    
+
+
+# user=input("ingrese su nombre de usuario")
+# if len(user)>=4 and len(user)<=10:
+#    print("muy pocos caracteres, use al menos 4")
+# elif len(user)>=10:
+#    print("tiene muchos caracteres, use 10 o menos")
+# else:
+#     print("usuario creado correctamente")
+
+
+# pin=input("cree su pin de 4 digitos")
+
+# if len(pin)==4:
+#     print("el pin esta correcto")
+# else:
+#     print("el pin esta incorrecto")
